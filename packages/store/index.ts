b1fac58d7cbb2c9eb9c90d23473cad7ca018e6ed
Torstein Thune <torstein@thune.io>
2025-02-17 10:56:26 +0100

    chore(store): cleanup

--- packages/store/index.ts
+++ packages/store/index.ts
@@ -1,9 +1,4 @@
 export * from "./src/Store";
 export * from "./src/useStore";
 export * from "./src/comparison";
-
-export type {
-  HashingFunction,
-  ActionHandler,
-  UpdateFunction,
-} from "./src/Store";
+export * from "./src/connectDevtools";
