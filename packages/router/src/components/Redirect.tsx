6797334af41fbe10b12f518e989d9917c198f518
Torstein Thune <torstein@thune.io>
2025-03-31 11:30:59 +0200

    fix(router): race in Redirect

--- packages/router/src/components/Redirect.tsx
+++ packages/router/src/components/Redirect.tsx
@@ -1,22 +1,32 @@
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 import { createRouteRegex, getParameters, replace } from "../helpers";
 import { useRoute } from "../hooks";
 import { inject } from "regexparam";
 import { resolve } from "../helpers/path";
 
-export function Redirect({ path, ...rest }: { path: string } & object) {
+/** **Redirect**
+ *
+ * Redirect to a different path.
+ *
+ * @param {string} path The path to redirect to.
+ *
+ * @example Redirect from a Route to another
+ *   <Route path="/old-path">
+ *    <Redirect path="/new-path" />
+ *  </Route>
+ */
+export function Redirect({ path }: { path: string } & object) {
   const { currentPath, routePath } = useRoute();
 
   const joinedPaths =
     path[0] === "~" ? path.slice(1) : resolve(routePath, path);
 
-  console.log("joinedPaths", joinedPaths);
-
-  useLayoutEffect(() => {
+  useEffect(() => {
     const { pattern, keys } = createRouteRegex(routePath);
     const params = getParameters(currentPath, pattern, keys);
+    const timeout = setTimeout(() => replace(inject(joinedPaths, params)), 0);
 
-    replace(inject(joinedPaths, params));
+    return () => clearTimeout(timeout);
   }, [joinedPaths, currentPath]);
   return <></>;
 }
