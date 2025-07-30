c46469bb8e58d7f453fd21ea4261bcdaac068207
Torstein Thune <torstein@thune.io>
2025-07-30 11:38:30 +0200

    docs(router): improve jsdoc for matchRoute

--- packages/router/src/helpers/matchRoute.ts
+++ packages/router/src/helpers/matchRoute.ts
@@ -1,10 +1,15 @@
 import { createRouteRegex } from "./createRouteRegex";
 
-/** matchRoute
+/** matchRoute(routePath, path)
  *
- * @param {string | RegExp} routePath
- * @param {string} path
- * @returns
+ * @param {string | RegExp} routePath - The route path to match against. E.g /some/:projectId/:taskId
+ * @param {string} path - The current path to match. E.g /some/123/456
+ * @returns {boolean} - Returns true if the path matches the route path. Otherwise false.
+ *
+ * @example
+ *   matchRoute("/some/:projectId/:taskId", "/some/123/456") // true
+ *   matchRoute("/some/:projectId/:taskId", "/some/123") // false
+ *
  */
 export const matchRoute = (routePath: string | RegExp, path: string) => {
   const pattern: RegExp =
