436e1e8faa9fe2872430e4ac6214b888b748769b
Snorre Magnus Davøen <post@snorre.io>
2026-02-27 12:57:50 +0100

    chore(bring): export type BringInit for external use (#8)
    Users of the library might want to define their own
    decorators/middleware for the bring pipes. Exporting this type makes it
    simpler.

--- packages/bring/src/index.ts
+++ packages/bring/src/index.ts
@@ -3,3 +3,5 @@ export * from "./tryFetch";
 export * from "./errors";
 export * from "./request-builder-pipe";
 export * from "./response-pipe";
+
+export type { BringInit } from "./types/BringInit";
