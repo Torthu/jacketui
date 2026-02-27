298d23a3ff5deb36bc22993e8189cab59749db4d
Snorre Magnus Davøen <snorre@ren.no>
2026-02-27 14:53:37 +0100

    chore(bring): Export type BringInitDecorator

--- packages/bring/src/index.ts
+++ packages/bring/src/index.ts
@@ -5,3 +5,4 @@ export * from "./request-builder-pipe";
 export * from "./response-pipe";
 
 export type { BringInit } from "./types/BringInit";
+export type { BringInitDecorator } from "./types/BringDecorator";
